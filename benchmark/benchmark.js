12ecb4cdebc5959bcae18d5aaa13335d220401bd
Quinn Slack <qslack@qslack.com>
2017-05-09 20:35:57 -0700

    wip

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -2,9 +2,9 @@
 
 const fs = require('fs')
 const path = require('path')
-const {OnigScanner} = require('..')
+const { OnigScanner } = require('..')
 
-function runBenchmarkSync (lines, scanner) {
+function runBenchmarkSync(lines, scanner) {
   let startTime = Date.now()
   let matches = 0
 
@@ -18,31 +18,10 @@ function runBenchmarkSync (lines, scanner) {
   console.log(`sync:  ${matches} matches in ${Date.now() - startTime}ms`)
 }
 
-function runBenchmarkAsync (lines, scanner) {
-  let matches = 0
-  let callsInProgress = 0
-
-  function callback (error, match) {
-    if (match != null) { matches++ }
-    if (--callsInProgress === 0) {
-      console.log(`async: ${matches} matches in ${Date.now() - startTime}ms`)
-    }
-  };
-
-  var startTime = Date.now()
-  for (let row = 0, rowCount = lines.length; row < rowCount; row++) {
-    const line = lines[row]
-    for (let position = 0, length = line.length; position <= length; position++) {
-      callsInProgress++
-      scanner.findNextMatch(line, position, callback)
-    }
-  }
-}
-
-console.log('oneline.js')
+console.log('medium.go')
 runBenchmarkSync(
-  fs.readFileSync(path.join(__dirname, 'oneline.js'), 'utf8').split('\n'),
-  new OnigScanner(['\\[', '\\]', '\\{', '\\}'])
+  fs.readFileSync(path.join(__dirname, 'medium.go'), 'utf8').split('\n'),
+  new OnigScanner(['\\(', '\\)', '\\{', '\\}', '\\/\\/'])
 )
 
 console.log('large.js')
@@ -51,7 +30,8 @@ runBenchmarkSync(
   new OnigScanner(['this', 'var', 'selector', 'window'])
 )
 
-runBenchmarkAsync(
-  fs.readFileSync(path.join(__dirname, 'large.js'), 'utf8').split('\n'),
-  new OnigScanner(['this', 'var', 'selector', 'window'])
+console.log('oneline.js')
+runBenchmarkSync(
+  fs.readFileSync(path.join(__dirname, 'oneline.js'), 'utf8').split('\n'),
+  new OnigScanner(['\\[', '\\]', '\\{', '\\}'])
 )
